7ee3c484ccc6c13b5c3cc9c7565cea1a11162bec
Alex <recompense.aps@gmail.com>
2019-10-22 19:42:35 -0600

    "menus" are now using godot UI system

--- Modes/LevelSelect.cs
+++ Modes/LevelSelect.cs
@@ -5,35 +5,33 @@ public class LevelSelect : Node2D
 {
     public override void _Ready()
     {
-        Util.GetButton(this, "CityButton").Connect("Click", this, nameof(OnCityButtonClick));
-        Util.GetButton(this, "JungleButton").Connect("Click", this, nameof(OnJungleButtonClick));
-        Util.GetButton(this, "OceanButton").Connect("Click", this, nameof(OnOceanButtonClick));
     }
 
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
+    //  // Called every frame. 'delta' is the elapsed time since the previous frame.
+    //  public override void _Process(float delta)
+    //  {
+    //      
+    //  }
 
-    private void OnCityButtonClick(Button button)
+    private void OnCityButtonPressed()
     {
         SetSpawnType("city");
         LaunchFreeFall();
     }
 
-    private void OnJungleButtonClick(Button button)
+
+    private void OnJungleButtonPressed()
     {
         SetSpawnType("jungle");
         LaunchFreeFall();
     }
 
-    private void OnOceanButtonClick(Button button)
+
+    private void OnOceanButtonPressed()
     {
         SetSpawnType("ocean");
         LaunchFreeFall();
     }
-
     private void LaunchFreeFall()
     {
         GetTree().ChangeScene("res://Modes/FreeFall.tscn");
